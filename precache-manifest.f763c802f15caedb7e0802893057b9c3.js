191cee27fd5d7d6b25bbcf67df8f281678b32200
Sam Ip <sip@MacBook-Pro.local>
2020-11-06 11:39:17 -0500

    Updates

---
+++ precache-manifest.f763c802f15caedb7e0802893057b9c3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "267e79440f1c57be518edd3e966c8082",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47739ddcd1a4794b1060",
+    "url": "/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "b8e1fbcaa5a92f7f0751",
+    "url": "/static/css/main.4a51c766.chunk.css"
+  },
+  {
+    "revision": "47739ddcd1a4794b1060",
+    "url": "/static/js/2.e63a5096.chunk.js"
+  },
+  {
+    "revision": "4c4db052c05148b648c7ca3a66f2cf37",
+    "url": "/static/js/2.e63a5096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "971ed28b8ea1f8121cff",
+    "url": "/static/js/3.5e4320c9.chunk.js"
+  },
+  {
+    "revision": "90a0a6d8e2c0a3eb858f",
+    "url": "/static/js/4.aca43c91.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/static/js/4.aca43c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dcf9872904fbb80eae5",
+    "url": "/static/js/5.c0f465f8.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.c0f465f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e1fbcaa5a92f7f0751",
+    "url": "/static/js/main.76a3c5e4.chunk.js"
+  },
+  {
+    "revision": "87c2d49e6bc43d85a65e",
+    "url": "/static/js/runtime-main.f77227d4.js"
+  }
+]);
