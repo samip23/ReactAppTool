6aaded2c66ae50413ed1c19dbfad4f703aa066ff
Kevin Baik <mphist@gmail.com>
2021-01-07 19:48:41 -0500

    fixed bugs in project list and added a graph for scenarios

--- client/src/Components/ScenarioItem.js
+++ client/src/Components/ScenarioItem.js
@@ -1,61 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { setResult } from "../redux/Scenario/action.js"
+import { setResult } from "../redux/Scenario/action.js";
 
-const ScenarioItem = ({ idx, scenario, onScenarioSelect, key }) => {
+const ScenarioItem = ({
+  projectId,
+  scenario,
+  onScenarioSelect,
+  projectKey,
+}) => {
+  const testResult = useSelector((state) => state.scenario)[projectId][
+    projectKey
+  ];
 
-    console.log("sceanriotem", scenario, idx)
-    
-    const result_by_idx = useSelector(state => {
-        if (state.scenario[idx] !== undefined) {
-            return state.scenario[idx].result;
-        }
-        return "";
-    })
-    
-    const [result, setResultS] = useState(result_by_idx);
-    const dispatch = useDispatch();
-    
-    function handleResultUpdate(e) {
-        setResultS(e.target.value)
-    };
-    
-    function handleButtonClick() {
-        if (result === "p" || result === "f" || result === "b") {
-            dispatch(setResult(idx, result, projectId));
-        }
-        else alert ("Invalid value was entered")
-    };
-    
-    return (
-        <div onClick={() => onScenarioSelect(idx)} className="item">
-            <div className="content">
-                <div class="header">
-                    <label style={{ cursor: "pointer" }}>Scenario Description: </label>
-                    {scenario.description}
-                </div>
-                <div class="description">
-                    <div>
-                        <label style={{ cursor: "pointer" }}>High Level Steps: </label>
-                        {scenario.highLevelSteps}
-                    </div>
-                    <div>
-                        <label style={{ cursor: "pointer" }}>Validation: </label>
-                        {scenario.validation}
-                    </div>
-                    <div>
-                        <label style={{ cursor: "pointer" }}>Language: </label>
-                        {scenario.language}
-                    </div>
-                    <div>
-                        <label>Test Result: </label>
-                        <input type="text" placeholder="Enter Result" value={result} onChange={handleResultUpdate} />
-                        <button type="submit" onClick={handleButtonClick}>Set Result</button>
-                    </div>
-                </div>
-            </div>
+  const [result, setResultS] = useState(testResult.result);
+  const dispatch = useDispatch();
+
+  function handleResultUpdate(e) {
+    setResultS(e.target.value);
+  }
+
+  function handleButtonClick() {
+    if (result === "p" || result === "f" || result === "b") {
+      setResultS(result);
+      dispatch(setResult(projectId, result, projectKey));
+    } else alert("Invalid value was entered");
+  }
+
+  return (
+    <div onClick={() => onScenarioSelect(projectId)} className="item">
+      <div className="content">
+        <div class="header">
+          <label style={{ cursor: "pointer" }}>Scenario Description: </label>
+          {scenario.description}
         </div>
-    )
-}
+        <div class="description">
+          <div>
+            <label style={{ cursor: "pointer" }}>High Level Steps: </label>
+            {scenario.highLevelSteps}
+          </div>
+          <div>
+            <label style={{ cursor: "pointer" }}>Validation: </label>
+            {scenario.validation}
+          </div>
+          <div>
+            <label style={{ cursor: "pointer" }}>Language: </label>
+            {scenario.language}
+          </div>
+          <div>
+            <label>Test Result: </label>
+            <input
+              type="text"
+              placeholder="Enter Result"
+              value={result}
+              onChange={handleResultUpdate}
+            />
+            <button type="submit" onClick={handleButtonClick}>
+              Set Result
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ScenarioItem;
